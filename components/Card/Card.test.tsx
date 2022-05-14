6a286350816b80b84d7b757fa79c2d724a499fed
Maximilian Paju <maximilian.paju@gmail.com>
2022-05-14 23:24:29 +0200

    feat: add card component

---
+++ components/Card/Card.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from "@testing-library/react";
+import { Card } from "./Card";
+
+describe("Card", () => {
+  it("should display a title in the card", () => {
+    render(<Card title={"Card title"} imageSrc="https://image-src.jpg" />);
+    const title = screen.getByTestId("card-title");
+    expect(title).toBeInTheDocument();
+  });
+
+  it("should display an image in the card", () => {
+    render(<Card title={"Card title"} imageSrc="https://image-src.jpg" />);
+    const title = screen.getByTestId("card-image");
+    expect(title).toBeInTheDocument();
+  });
+
+  it("should render any children", () => {
+    render(
+      <Card title={"Card title"} imageSrc="https://image-src.jpg">
+        <p data-testid="card-child">Child content</p>
+      </Card>
+    );
+    const child = screen.getByTestId("card-child");
+    expect(child).toBeInTheDocument();
+  });
+});
---
+++ components/Card/Card.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import { HTMLProps } from "react";
+import styles from "./Card.module.scss";
+
+type CardProps = {
+  title: string;
+  imageSrc: string;
+} & HTMLProps<HTMLDivElement>;
+
+export function Card({
+  title,
+  imageSrc,
+  className = "",
+  children = <></>,
+  ...props
+}: CardProps) {
+  return (
+    <div className={`${styles.card} ${className}`} {...props}>
+      <Image
+        className={styles.cardImage}
+        data-testid="card-image"
+        src={imageSrc}
+        objectFit="cover"
+        layout="fixed"
+        width="200"
+        height="200"
+        alt={title}
+      />
+      <div className={styles.cardBody}>
+        <h3 className={styles.cardTitle} data-testid="card-title">
+          {title}
+        </h3>
+
+        {children}
+      </div>
+    </div>
+  );
+}
