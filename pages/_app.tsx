b5cbffd2c7f567e30b7e860fd1aa409657b34531
Maximilian Paju <maximilian.paju@gmail.com>
2022-05-14 20:09:09 +0200

    chore: clean up layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,14 @@ import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="layout">
+      <main>
+        <Component {...pageProps} />
+      </main>
+      <footer></footer>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
