083dc1469c2f8c1332053465c6964c2849329e79
Maximilian Paju <maximilian.paju@gmail.com>
2022-05-16 09:43:22 +0200

    feat: redirect from / to /products

--- next.config.js
+++ next.config.js
@@ -1,8 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  poweredByHeader: false,
   reactStrictMode: true,
   images: {
     domains: ["fakestoreapi.com"],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/products",
+        permanent: true,
+      },
+    ];
   },
 };
 
